d095395d3a4f4b93c496f99a08b7d47c2bb446df
JustinChrisAmbil <chrisambil27@gmail.com>
2022-08-18 11:06:07 +0800

    Changes

---
+++ src/Components/Nbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from ' ../Images/Royalty.png';
+function Nbar() {
+return (
+        <div className="nbar">
+            <div className="lside">
+            <img src={Logo} />
+            </div>
+            <div className="rside"/>
+        
+    </div>
+)
+}
+
+export default Nbar
