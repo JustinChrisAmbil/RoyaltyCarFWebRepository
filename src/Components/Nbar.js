39dcc36defdf55484fecd6bfa617fe100baa501a
JustinChrisAmbil <chrisambil27@gmail.com>
2022-08-18 12:37:56 +0800

    CSS Styles for HomeP

--- src/Components/Nbar.js
+++ src/Components/Nbar.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import Logo from "../Images/Royalty.png";
 import {Link} from "react-router-dom";
-
+import '../style/Nbar.css';
 function Nbar() {
 return (
         <div className="nbar">
-            <div className="lside">
+            <div className="lSide">
             <img src={Logo} />
             </div>
-            <div className="rside"/>
+            <div className="rSide"/>
                 <Link to ="/"> Home</Link>
                 <Link to ="/Buy A Car"> Buy A Car</Link>
                 <Link to ="/ Sell A Car"> Sell A Car</Link>
                 
     </div>
-)
+);
 }
 
 export default Nbar
